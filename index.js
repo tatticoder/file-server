23123d109a24c27df3772b27b1c3f28de49175dd
Naman Singhal <nsnamansinghal7@gmail.com>
2020-12-19 01:43:12 +0530

    read files in directory but not subdirectories

--- index.js
+++ index.js
@@ -1,47 +1,62 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
-
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+//disable express headers
+app.disable("x-powered-by");
+//replace with path of folder you want to read
+const directoryPath = "./";
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  //passsing directoryPath and callback function
+  fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+      res.render("error", { err: JSON.stringify(err) });
+      return console.log("Unable to scan directory: " + err);
+    }
+    res.render("index", { list: files });
+  });
 });
-
+app.get("/play", (req, res) => {
+  console.log("Name of file is " + req.query.name);
+  res.render("play", { file: req.query.name });
+});
 app.get("/video", function (req, res) {
   // Ensure there is a range given for the video
   const range = req.headers.range;
   if (!range) {
     res.status(400).send("Requires Range header");
   }
-
-  // get video stats (about 61MB)
-  const videoPath = "12.mp4";
+  const videoPath = directoryPath + req.query.name;
   const videoSize = fs.statSync(videoPath).size;
+  try {
+    // Parse Range
+    // 1MB or 1% of video whicever is larger
+    const CHUNK_SIZE =
+      10 ** 6 > videoSize / 100 ? 10 ** 6 : Math.floor(videoSize / 100);
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1); // Create headers
+    const contentLength = end - start + 1;
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": contentLength,
+      "Content-Type": "video/mp4",
+    };
 
-  // Parse Range
-  // Example: "bytes=32324-"
-  // 1MB or 1% of video whicever is larger
-  const CHUNK_SIZE =
-    10 ** 6 > videoSize / 100 ? 10 ** 6 : Math.floor(videoSize / 100);
-  const start = Number(range.replace(/\D/g, ""));
-  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+    // HTTP Status 206 for Partial Content
+    res.writeHead(206, headers);
 
-  // Create headers
-  const contentLength = end - start + 1;
-  const headers = {
-    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-    "Accept-Ranges": "bytes",
-    "Content-Length": contentLength,
-    "Content-Type": "video/mp4",
-  };
+    // create video read stream for this particular chunk
+    const videoStream = fs.createReadStream(videoPath, { start, end });
 
-  // HTTP Status 206 for Partial Content
-  res.writeHead(206, headers);
-
-  // create video read stream for this particular chunk
-  const videoStream = fs.createReadStream(videoPath, { start, end });
-
-  // Stream the video chunk to the client
-  videoStream.pipe(res);
+    // Stream the video chunk to the client
+    videoStream.pipe(res);
+  } catch (error) {
+    console.error("error is " + error);
+    res.status(400).render("error", error);
+  }
 });
 
 app.listen(8000, function () {
