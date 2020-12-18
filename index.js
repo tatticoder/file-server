2cc33186c696179564b94275a81697269e75bf13
Naman Singhal <nsnamansinghal7@gmail.com>
2020-12-19 02:17:23 +0530

    can recursively scan directories but not open file

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ app.use(express.static(__dirname + "/public"));
 //disable express headers
 app.disable("x-powered-by");
 //replace with path of folder you want to read
-const directoryPath = "./";
+const directoryPath = "/media/stardust/songs/epicfiles/porn/";
 app.get("/", function (req, res) {
   //passsing directoryPath and callback function
-  fs.readdir(directoryPath, function (err, files) {
+  if (req.query.name) {
+    console.log("PLease scan :" + req.query.name);
+    //for subfolders in this folder
+    var directoryPath2 = directoryPath + req.query.name;
+  } else {
+    var directoryPath2 = directoryPath;
+  }
+  fs.readdir(directoryPath2, function (err, files) {
     //handling error
     if (err) {
       res.render("error", { err: JSON.stringify(err) });
@@ -20,7 +27,15 @@ app.get("/", function (req, res) {
 });
 app.get("/play", (req, res) => {
   console.log("Name of file is " + req.query.name);
-  res.render("play", { file: req.query.name });
+
+  if (fs.lstatSync(directoryPath + req.query.name).isDirectory()) {
+    console.log("it is a directory :" + req.query.name);
+    res.redirect(`/?name=${req.query.name}`);
+  } else {
+    //currently only for videos, more formats will be supported in future
+    console.log("it is a directory :" + req.query.name);
+    res.render("play", { file: req.query.name });
+  }
 });
 app.get("/video", function (req, res) {
   // Ensure there is a range given for the video
