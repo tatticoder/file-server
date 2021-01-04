c6b20f526052cad66fe47564e0bd84f07b1814b4
Naman Singhal <nsnamansinghal7@gmail.com>
2021-01-04 14:20:00 +0530

    Add support for PDF files

--- routes/identify.js
+++ routes/identify.js
@@ -2,19 +2,27 @@ const fs = require("fs");
 const path = require("path");
 exports.send = (req, res) => {
   const resPath = req.query.path + req.query.name;
-  if (fs.lstatSync(resPath).isDirectory()) {
-    console.log("User requested directory :" + resPath);
-    res.redirect(`/?name=${resPath}`);
-  } else {
-    const ext = path.extname(resPath);
-    if ([".mp4", ".mkv"].includes(ext)) {
-      res.render("playvideo", { file: resPath });
-    } else if ([".mp3", ".flac", ".m4a"].includes(ext)) {
-      res.render("playsong", { file: resPath });
-    } else if (
-      [".jpg", ".jpeg", ".png", ".gif", ".svg", ".webp"].includes(ext)
-    ) {
-      res.render("showimage", { file: resPath });
+  try {
+    if (fs.lstatSync(resPath).isDirectory()) {
+      console.log("User requested directory :" + resPath);
+      res.redirect(`/?name=${resPath}`);
+    } else {
+      const ext = path.extname(resPath);
+      if ([".mp4", ".mkv"].includes(ext)) {
+        res.render("playvideo", { file: resPath });
+      } else if ([".mp3", ".flac", ".m4a"].includes(ext)) {
+        res.render("playsong", { file: resPath });
+      } else if (
+        [".jpg", ".jpeg", ".png", ".gif", ".svg", ".webp"].includes(ext)
+      ) {
+        res.render("showimage", { file: resPath });
+      } else if (".pdf" == ext) {
+        res.render("document", { file: resPath });
+      } else {
+        res.render("error", { err: ext + " is not supported" });
+      }
     }
+  } catch (error) {
+    res.status(500).render("error", { err: error });
   }
 };
