fc7ac0bad5861002f04e4bd43c60bb2018063415
Naman Singhal <nsnamansinghal7@gmail.com>
2021-02-05 22:32:54 +0530

    create back pressured streams for download

--- routes/download.js
+++ routes/download.js
@@ -1,7 +1,8 @@
+const fs = require("fs");
 exports.download = (req, res) => {
   try {
-    console.log("User requested file :", req.query.name);
-    res.sendFile(req.query.name);
+    console.log("User wants :", req.query.name);
+    fs.createReadStream(req.query.name).pipe(res);
   } catch (err) {
     console.error(err);
     res.render("error", { err: JSON.stringify(err) });
